b93bdf1025d612c032e7099ae3c386dd735c3d51
Ben Hensor <benhensor@gmail.com>
2023-08-11 14:47:31 +0100

    state management and api call set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  console.log("Hello World!")
+
+  const [ data, setData ] = useState([{}])
+
+
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("http://localhost:5000/fizzbuzz")
+          const data = await response.json()
+          setData(data)
+          console.log(data)
+        } catch (error) {
+          console.error("Error fething data:", error)
+        }
+      }
+
+      fetchData()
+      
+  }, [])
+
+
   return (
     <main className="App">
       
