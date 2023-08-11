f3f514fc9792d845f1cfa822d083920c73ac08d4
Ben Hensor <benhensor@gmail.com>
2023-08-11 16:05:16 +0100

    styles complete. comments added to server and client files

--- client/src/App.js
+++ client/src/App.js
@@ -2,52 +2,115 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
+// Main App component
+
+// Here useEffect is deployed to fetch data from the API and set it to state on load.
+// The call is checked for a valid response.
+// If the response is not valid, a loading message is displayed.
+// The loading message is displayed until the response is valid.
+// If the response is valid, the data is mapped to the page in the form of a paragraph element.
+// The class name of the paragraph element is set by the getClass function.
+// The getClass function checks the value of the number and returns a class name based on the value.
+// The class name is then used to style the paragraph element.
+
+
+
 function App() {
 
-  console.log("Hello World!")
+  // Log to check React is working
+  console.log("React App Launched - Hello World!")
 
+
+  // Set up state for data from API
   const [ data, setData ] = useState([{}])
 
 
+  // The simplest solution seemed to suggest using useEffect to make the API call 
+  // when the component mounts. I could have implemented a button to make the call
+  // but this option works well for this use-case.
+
+
+  // Fetch data from API
   useEffect(() => {
       const fetchData = async () => {
         try {
+          // Asynchronous fetch request to API
           const response = await fetch("http://localhost:5000/fizzbuzz")
+          // Convert response to JSON
           const data = await response.json()
+          // Set data to state
           setData(data)
-          console.log(data)
+          // Log data to console to check it is working
+          console.log("API Response", data)
         } catch (error) {
+          // Log any errors
           console.error("Error fething data:", error)
         }
       }
 
+      // Call fetchData function
       fetchData()
 
   }, [])
+
+
+  // As I was approaching the styling of the page, I thought it would be interesting 
+  // to have the option of styling each element of the fizzbuzz result differently.
+
+
+  // Function to set class name of paragraph element
+  function getClass(number) {
+
+    if (number === "Fizz") {
+      return "fizzStyle";
+    }
+    if (number === "Buzz") {
+      return "buzzStyle";
+    }
+    if (number === "FizzBuzz") {
+      return "fizzBuzzStyle";
+    }
+    return "numberStyle";
+  }
+
+
+  // Return the page
+  // I have opted to dynamically map the data results using the map() method.
+  // This is a good code-efficient solution as it allows for the entirety of 
+  // the data to be displayed whilst reducing the need to hard-code everything.
+
+  // On a more-or-less purely aesthetic level, I have added headings with some links
+  // just for fun. I think it makes the page more interesting to view.
+
 
 
   return (
     <main className="App">
       <section>
 
-      <article>
+      <article id="header">
       <h1><a href="https://blckbx.co.uk/" rel="noreferrer" target="_blank">BLCK <span>BX</span></a></h1>
         <p><span>Technical Test</span></p>
       </article>
-      <article>
+      <article id="subheader">
       <h2>Full Stack Developer - </h2>
-      <h3><a href="https://benhensordev.netlify.app/" rel="noreferrer" target="_blank">Ben Hensor</a></h3>
+      <h3><a href="https://benhensordev.netlify.app/" rel="noreferrer" target="_blank">ben hensor</a></h3>
       </article>
+      <hr />
+
+      <div>
 
         {( typeof data.result === 'undefined' ) ? (
             <p>Loading...</p>
         ) : (
             data.result.map((number, i) => (
-                <p key={i}>
+                <p key={i} className={getClass(number)}>
                   {number}
                 </p>
             ))
         )}
+
+      </div>
 
         </section>
     </main>
--- server/server.py
+++ server/server.py
@@ -1,31 +1,75 @@
 from flask import Flask
 from flask_cors import CORS
 
+# I've not used Python before so I had to do a bit of learning and research
+# before designing this solution. Using vanilla python to create endpoints looked
+# quite challenging and so I opted to use Flask and Cors as it seemed to make 
+# the process a little easier. The syntax is quite similar to Node.js and Express
+# which I am more familiar with. I also used the Flask documentation to help me.
 
+
+
+# Initialise app
 app = Flask(__name__)
+# Add CORS support for communication between front and back end ports
 CORS(app)
 
 
+# Check initialisation
+print("Initialising Flask app...")
+
+# Once the server packages are initialised, the endpoint for the API can be defined.
+# Then, define a function to be called when the endpoint is hit.
+# This function computates the fizzbuzz rules and erturns the formatted number list as json.
+
+
+# Define route handler
 @app.route("/fizzbuzz")
+
+# Fn to calculate fizzbuzz rules
 def fizzbuzz():
     
+    # Check endpoint is hit
+    print("Endpoint: /fizzbuzz hit successful")
+    
+    # Create and empty array to store the result
     result = []
 
-    for number in range(1, 101):
 
+    # Loop through numbers 1 to 100
+    for number in range(1, 101):
+        
+        # Check if number is divisible by 3, 5 or both
         if number % 3 == 0 and number % 5 == 0:
+            
+        # If divisible by both, append FizzBuzz to the result array
           result.append("FizzBuzz")
+
+        # Check if divisible by 3
         elif number % 3 == 0:
+            
+        # If divisible by 3, append Fizz to the result array
           result.append("Fizz")
+
+        # Check if divisible by 5
         elif number % 5 == 0:
+
+        # If divisible by 5, append Buzz to the result array
           result.append("Buzz")
+
         else:
+        # If not divisible by 3 or 5, append the number to the result array
           result.append(number)
 
+    # Check result
+    print("fizzbuzz result: ", result)
 
+    # Return the result array as JSON
     return { "result": result }
 
 
-
+# Run the app
 if __name__ == "__main__":
+    
+    # Ensure that the server is being run in dev mode on the correct port
     app.run(host='0.0.0.0', port=5000)
